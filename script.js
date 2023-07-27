cf578bfaaf7377352c686f39f0b24669bf60771c
adelinebelova <63752238+adelinebelova@users.noreply.github.com>
2023-07-27 10:37:52 -0400

    Add contact section and footer. Add scrolling links to buttons.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let adeline = document.getElementById("name"); 
 let workResume = document.getElementById("work-resume");
 let musicResume = document.getElementById("music-resume");
-
+let emailIcon = document.getElementById("email");
 
 
 //add gradient sheen effect when hovering over text
@@ -13,4 +13,10 @@ adeline.addEventListener("animationend", function removeGradient() {
     adeline.classList.remove("gradient");
 });
 
+//copy link popup 
+emailIcon.addEventListener('click', function copyLink() {
+    navigator.clipboard.writeText(emailIcon.classList);
 
+        //SweetAlert popup
+        swal("Email Copied: adelinebelova@yahoo.com");
+});
